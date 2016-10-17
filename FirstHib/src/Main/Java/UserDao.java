5a9d75749a71dc07a9e7f742debe000cdc59618c
Licensent <DarthVader.1@mail.ru>
2016-10-17 22:46:24 +0300

    userDao change package to public

--- FirstHib/src/Main/Java/UserDao.java
+++ FirstHib/src/Main/Java/UserDao.java
@@ -2,6 +2,6 @@
  * Created by DarthVader on 05.10.2016.
  */
 public interface UserDao {
-    public void addUsers(User user);
-    public void updateUser(String foundName, String changeName, Integer age);
+    void addUsers(User user);
+    void updateUser(String foundName, String changeName, Integer age);
     }
